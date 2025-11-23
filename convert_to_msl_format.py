29cc8926642a803f1527247ed6e5ad92cd6ab6a8
ensi <ensi.dehghani@avestaconsulting.net>
2025-11-23 11:54:59 +1100

    second commit

--- convert_to_msl_format.py
+++ convert_to_msl_format.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88e467705d594499ae8c902aa5e5d3e7d73c7c211d100eb567224f2ee5383dc1
-size 10607
+oid sha256:1b47d88cf58c22f200e21e2cd7a06f67e22b867b2cd56190c37e025ddcab3357
+size 10533
---
+++ predictive_fault_detection.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1006c34d86e9ef255b2e3b9f1aae8b310f06210b01d575d5fe911bfc65883731
+size 8549
